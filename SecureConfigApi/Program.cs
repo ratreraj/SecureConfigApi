c756513993e31820f9193caa7a1e3e54dd041922
rajratre <rajprakashratre@gmail.com>
2026-01-19 19:38:54 +0530

    Add project files.

---
+++ SecureConfigApi/Program.cs
@@ -0,0 +1,29 @@
+using Microsoft.Identity.Web;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddAuthentication("Bearer")
+    .AddMicrosoftIdentityWebApi(builder.Configuration);
+builder.Services.AddAuthentication();
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
